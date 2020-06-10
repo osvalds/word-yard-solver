b3b70b9fe86eb412ef845049a1650d0b0b6b4816
osvalds <osvalds.neiders@gmail.com>
2020-06-11 00:18:48 +0300

    Updates

---
+++ precache-manifest.855748dfd09e3944abcf37ceac3ddf7b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8aff36cfb96fddf55afa920e6e4223",
+    "url": "/word-yard-solver/7a64ec12ecf5e6cab25e.worker.js"
+  },
+  {
+    "revision": "e16335b395471c4a73e34467a1728feb",
+    "url": "/word-yard-solver/index.html"
+  },
+  {
+    "revision": "e9c530bc245296e31307",
+    "url": "/word-yard-solver/static/css/main.18076a82.chunk.css"
+  },
+  {
+    "revision": "be0a414dc64563e9a1c2",
+    "url": "/word-yard-solver/static/js/2.cf0d4ed8.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/word-yard-solver/static/js/2.cf0d4ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c530bc245296e31307",
+    "url": "/word-yard-solver/static/js/main.558874d4.chunk.js"
+  },
+  {
+    "revision": "d88b95c0d2dea783c1d3",
+    "url": "/word-yard-solver/static/js/runtime-main.4b9d17bd.js"
+  },
+  {
+    "revision": "43057faaa113978d1597a23b67bcd4e4",
+    "url": "/word-yard-solver/static/media/add.43057faa.svg"
+  },
+  {
+    "revision": "0069b3dc882199f0973e750af5dd4aa7",
+    "url": "/word-yard-solver/static/media/blank-check-box.0069b3dc.svg"
+  },
+  {
+    "revision": "a1750798526a9e7df22eec5aae31330b",
+    "url": "/word-yard-solver/static/media/check-box.a1750798.svg"
+  },
+  {
+    "revision": "52494273d1305e7ff6a6c050478da3dc",
+    "url": "/word-yard-solver/static/media/clear.52494273.svg"
+  },
+  {
+    "revision": "31c6d96b70f58b0cf3d06ce8263bec5b",
+    "url": "/word-yard-solver/static/media/remove.31c6d96b.svg"
+  }
+]);
